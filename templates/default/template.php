814842e8b3120463d80da65b4e9f158a0161bf6e
Kilian kreibich <mail@kilian-kreibich.de>
2020-01-01 14:04:30 +0100

    started template structure

---
+++ templates/default/template.php
@@ -0,0 +1,15 @@
+<?php
+
+//Template Metadata
+$tempMeta = [
+    "name" => "",
+    "desc" => "",
+    "version" => "",
+    "owner_name" => "",
+    "owner_mail" => ""
+];
+
+//Function that renders template
+function showTemp(){
+    //TODO: Load template
+}
