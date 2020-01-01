e4d6cbf5e1604ee2496d688753228f86b6523349
Kilian kreibich <mail@kilian-kreibich.de>
2020-01-01 13:37:05 +0100

    Added default configuration Added Redirects to Mainpage from subfolders Changed structure in templates folder

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+// KCMS Configuration File
+// Includes DB-Configuration
+
+$dbconfig = [
+    "server" => "localhost",
+    "username" => "DEFAULT",
+    "password" => "DEFAULT",
+    "database" => "DEFAULT",
+    "prefix" => "kcms_"
+];
---
+++ includes/libloader.php
@@ -0,0 +1,6 @@
+<?php
+//Include PHP Libraries
+include("./dblib.php");
+include("./medialib.php");
+include("./contentlib.php");
+include("./templatelib.php");
