7384373e51b58753e0f65d03279617cff594bce5
Kilian kreibich <mail@kilian-kreibich.de>
2020-01-02 13:56:50 +0100

    Improved Redirects

--- media_static/index.php
+++ media_static/index.php
@@ -1,2 +1,6 @@
-<?php 
-header("Location: /index.php", true ,301);
+<?php
+$host  = $_SERVER['HTTP_HOST'];
+$uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+$extra = 'index.php';
+header("Location: http://$host$uri/$extra");
+exit;
