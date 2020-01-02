475351f3f2fc0d1446c3db5766c7bbe9a6586645
Kilian kreibich <mail@kilian-kreibich.de>
2020-01-02 17:02:58 +0100

    Fixed prev. Commit

--- includes/dblib.php
+++ includes/dblib.php
@@ -134,7 +134,7 @@ function setConfigData(string $conf_name, string $conf_data){
 	global $conn;
 	$table = $tables["config"];
 	if(confExists($conf_name)){
-		$q = "UPDATE `".$table."` SET `conf_val` = '". $conn->real_escape_string($conf_name) ."' WHERE `conf_name` = '".$conn->real_escape_string($conf_data)."'";
+		$q = "UPDATE `".$table."` SET `conf_val` = '". $conn->real_escape_string($conf_data) ."' WHERE `conf_name` = '".$conn->real_escape_string($conf_name)."'";
 	} else {
 		$q = "INSERT INTO `".$table."` (`conf_name`, `conf_val`) VALUES ('".$conn->real_escape_string($conf_name)."','".$conn->real_escape_string($conf_data)."')";
 	}
